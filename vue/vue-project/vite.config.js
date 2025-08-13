e41d53b60a6d6ae7146af2734d978c8a4b4c1ebb
Joseph <joseph.d.ruizz@gmail.com>
2025-08-13 12:02:34 -0400

    Change file directory

--- vue/vue-project/vite.config.js
+++ vue/vue-project/vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +16,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: path.resolve(__dirname, '../djangofiles/static/frontend'), // where files go after build
+    emptyOutDir: true, // clean it before each build
+    manifest: true     // useful if Django templates want to use hashed file names
+  }
 })
