2717e8b8e4a963a436f44c707bab34c98a0256f0
Joseph <joseph.d.ruizz@gmail.com>
2025-08-13 00:50:44 -0400

    added Vue, no major changes yet. no adjustments to settings

---
+++ djangofiles/vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+const counting = computed(() => {
+  return count.value > 1 ? 'positive' : 'negative'
+})
+</script>
+
+<template>
+  <button @click="increment">
+    {{ count }}
+  </button>
+
+  <p>count?</p>
+  <span>{{ counting }}</span>
+</template>
