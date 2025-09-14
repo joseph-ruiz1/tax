a7530b24697d9b0de304ecfd220e4ffb8e73cd57
Joseph <joseph.d.ruizz@gmail.com>
2025-09-14 11:46:31 -0400

    .env for vue. in main.js

--- vue/vue-project/src/main.js
+++ vue/vue-project/src/main.js
@@ -4,13 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-
-axios.defaults.baseURL = 'http://localhost:8000/tax/api/'
-axios.defaults.withCredentials = true
-axios.defaults.withXSRFToken = true
-axios.defaults.xsrfCookieName = 'csrftoken'
-axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
 import VueApexCharts from "vue3-apexcharts";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
+
 
 
 createApp(App).use(router).use(VueApexCharts).mount('#app')
