5051a64b1f73f9a033c2b2fb89575b83a56239df
Joseph <joseph.d.ruizz@gmail.com>
2025-09-16 23:33:51 -0400

    remove dupe axios URL root

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -21,8 +21,6 @@ const getApiUrl = () => {
 };
 
 axios.defaults.baseURL = getApiUrl();
-
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 axios.defaults.xsrfCookieName = 'csrftoken';
