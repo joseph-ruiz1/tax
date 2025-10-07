56c6f9c2d725b4c941c1b97fd2f66abcd7dea9c8
Joseph <joseph.d.ruizz@gmail.com>
2025-10-07 16:23:13 -0400

    static.json to redirect to work around CORS

--- djangofiles/mysite/settings.py
+++ djangofiles/mysite/settings.py
@@ -98,12 +98,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
 "http://localhost:5173",
-"https://tax-vue.onrender.com",
 "https://tax-83z6.onrender.com"
 ]
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
-    "https://tax-vue.onrender.com",
     "https://tax-83z6.onrender.com"
 ]
 
