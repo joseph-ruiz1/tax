320dbae0c26f4426b937d30ce1a71489dfbff30c
Joseph <joseph.d.ruizz@gmail.com>
2025-10-22 22:42:44 -0400

    updating vite.config.js settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,30 +6,38 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    vue(),
-    // Only use devtools in local development
-    mode === 'local' && vueDevTools(),
-  ].filter(Boolean),
-
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(async ({ mode }) => {
+  const plugins = [vue()]
+  
+  // Only import and use devtools in local development
+  if (mode === 'local') {
+    const vueDevTools = (await import('vite-plugin-vue-devtools')).default
+    plugins.push(vueDevTools())
+  }
+  
+  return {
+    plugins,
+    
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  build: {
-    outDir: 'dist', // where files go after build
-    emptyOutDir: true, // clean it before each build
-    manifest: true     // useful if Django templates want to use hashed file names
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000/tax/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // important!
+    
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      manifest: true
+    },
+    
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8000/tax/api',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
       }
     }
   }
-}))
+})
