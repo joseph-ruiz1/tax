5c223d42e96a7b4bbfec43dec2428f6ea4c8b4ad
Joseph <joseph.d.ruizz@gmail.com>
2025-10-24 22:52:52 -0400

    remove isAdmin from DEFAULT_PERMISSIONS_CLASSES in settings.py

--- djangofiles/mysite/settings.py
+++ djangofiles/mysite/settings.py
@@ -71,7 +71,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
    ],
    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser',
         'rest_framework.permissions.IsAuthenticated',
    ),
 }
