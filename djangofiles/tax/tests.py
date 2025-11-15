2de057dd8b8c7881b766836859ab05b9f0c5d027
Joseph <joseph.d.ruizz@gmail.com>
2025-11-15 10:47:46 -0500

    Update several tests

--- djangofiles/tax/tests.py
+++ djangofiles/tax/tests.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APITestCase
@@ -53,8 +54,8 @@ def create_tax_year_data_list(test_cases, user, save=True):
                      is_electing=True, elected_farm_income=20000, qualified_farm_income=1000),
             ],
             'outputs': [143, 2812],
-            'dataset': dict(name='test', max_elected_farm_income=10000, qualified=0)
-        ]
+            'dataset': dict(name='test', max_elected_farm_income=10000, qualified=0)}
+        ],
     
     Also supports compact list style:
         [
@@ -85,7 +86,6 @@ def create_tax_year_data_list(test_cases, user, save=True):
             dataset.save()
             # Get the Decimal typing instead of int
             dataset.refresh_from_db()
-
 
      # --- create TaxYearData instances linked to dataset ---
         raw_inputs = case.get("inputs", [])
@@ -135,10 +135,6 @@ class TestUserModel(TestCase):
         self.assertEqual(test_user.id, 1)
 
     def test_create_several_users(self):
-        """
-        
-        """
-        
         users = [create_several_test_users(username, password) for username, password in CREDENTIALS]
 
         self.assertEqual(User.objects.count(), len(CREDENTIALS))
@@ -147,105 +143,11 @@ class TestUserModel(TestCase):
         actual_ids = [user.id for user in users]
         self.assertEqual(actual_ids, expected_ids)
 
-class TaxDataSetModelTest(TestCase):
-    """
-    Testing the InputsCreateView and validates a few tax calcs
-    """
-    def setUp(self):
-        self.test_user = create_test_user(username="test", password="testing")
-        self.client.login(username="test", password="testing")
-                        
-    def test_load_single_set_with_years(self):
-        """
-        Loads a single TaxDataSet instance into the view from dashboard and checks that all years are input properly
-        """
-        response = self.client.get(reverse("tax:start-dataset"))
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.post(reverse("tax:start-dataset"), data={"name": "test", "max_elected_farm_income": 10000, "qualified_farm_income": 1000}, follow=True)
-        self.assertEqual(TaxDataSet.objects.filter(user=self.test_user).count(), 1)
-        self.assertEqual(response.status_code, 200)
-
-        # Redirect
-        dataset = TaxDataSet.objects.filter(user=self.test_user).latest("id")
-        input_url = reverse("tax:inputs", kwargs={"dataset_pk": dataset.pk})
-
-        tax_data_inputs = [
-            (2024, "single", 100000, 40000,),
-            (2023, "MFJ", 20000, 100,),
-            (2022, "MFJ", 2000, 50,),
-            (2021, "single", 100000, 10000,),
-        ]
-        
-        formset_data = build_taxyear_formset_data(tax_data_inputs)
-
-        response = self.client.post(input_url, formset_data, follow=True)
-        
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(TaxDataSet.objects.filter(user=self.test_user).count(), 1, "uhhh")
-        self.assertEqual(TaxYearData.objects.filter(dataset__user=self.test_user).count(), 4, "Not saved to DB")
-        self.assertEqual(TaxYearData.objects.filter(dataset__user=self.test_user).first().year, "2024")
-        
-
-
-    def test_load_multiple_sets_check_base_calculations(self):
-        """
-        Validates that all base year calculations are correct
-        """
-        # Create first set and POST initial data
-        response = self.client.get(reverse("tax:start-dataset"))
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.post(reverse("tax:start-dataset"), data={"name": "test", "max_elected_farm_income": 10000, "qualified_farm_income": 1000}, follow=True)
-        self.assertEqual(TaxDataSet.objects.filter(user=self.test_user).count(), 1)
-        self.assertEqual(response.status_code, 200)
-
-        # Redirect
-        dataset = TaxDataSet.objects.filter(user=self.test_user).latest("id")
-        input_url = reverse("tax:inputs", kwargs={"dataset_pk": dataset.pk})
-
-        # Formset we want to POST
-        tax_data_inputs = [
-            (2024, "single", 100000, 40000,),
-            (2023, "MFJ", 20000, 100,),
-            (2022, "MFJ", 2000, 50,),
-            (2021, "single", 100000, 10000,),
-        ]
-        
-        formset_data = build_taxyear_formset_data(tax_data_inputs)
-        # POST that formset
-        response = self.client.post(input_url, formset_data, follow=True)
-        self.assertEqual(response.status_code, 200)
-
-        # Create Second Set
-        response = self.client.get(reverse("tax:start-dataset"))
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.post(reverse("tax:start-dataset"), data={"name": "test2", "max_elected_farm_income": 5000, "qualified_farm_income": 40}, follow=True)
-        self.assertEqual(TaxDataSet.objects.filter(user=self.test_user).count(), 2)
-        self.assertEqual(response.status_code, 200)
-
-        # Redirect
-        dataset = TaxDataSet.objects.filter(user=self.test_user).latest("id")
-        input_url = reverse("tax:inputs", kwargs={"dataset_pk": dataset.pk})
-
-        tax_data_inputs = [
-            (2024, "MFJ", 120000, 105000,),
-            (2023, "MFJ", 85000, 70000,),
-            (2022, "single", 55000, 40000,),
-            (2021, "MFJ", 96000, 45000,)
-        ]
-
-        formset_data = build_taxyear_formset_data(tax_data_inputs)
-        response = self.client.post(input_url, formset_data, follow=True)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(TaxYearData.objects.filter(dataset__pk=dataset.pk).count(), 4, "Second set of years not saved to DB")
-        self.assertEqual(TaxDataSet.objects.filter(user=self.test_user).count(), 2, "Sets not saved to DB")
-        self.assertEqual(TaxYearData.objects.filter(dataset__user=self.test_user).count(), 8, "All TaxYearDatas not saved to DB")
-
 
 class BaseTaxCalculationsTest(TestCase):
+    """
+    Run base TaxCalculation and compare to outputs in TEST_CASES
+    """
     def setUp(self):
         self.test_user = create_test_user(username="test", password="testing")
         self.client.login(username="test", password="testing")
@@ -266,51 +168,66 @@ class BaseTaxCalculationsTest(TestCase):
                 if round(result) != expected:
                     print(f"❌ Test {i}, Year {j}: Got {result}, expected {expected}")
 
+class CalculationsTest(TestCase):
+    """
+    Run base TaxCalculation to test 2021-2018. Results should match TEST outputs
+    """
+    def setUp(self):
+        self.test_user = create_test_user(username="test", password="testing")
+        self.client.login(username="test", password="testing")
+
+    def test_base_tax_calculations(self):
+        # Create test sets
+        for i, test in enumerate(TEST_OLDER_YEARS):
+            dataset = create_dataset_with_tax_years(self.test_user, test['inputs'], 0, 0)
+
+            total_tax_results = []
+            # Base calculations
+            for tax_year in TaxYearData.objects.filter(dataset=dataset):
+                TaxCalculation(tax_year).calculate()
+                total_tax_results.append(round(tax_year.total_tax))
+
+            # Compare to expected
+            for j, (result, expected) in enumerate(zip(total_tax_results, test['outputs'])):
+                if round(result) != expected:
+                    print(f"❌ Test {i}, Year {j}: Got {result}, expected {expected}")
 
 class ScheduleJCalculationTest(TestCase):
+    """
+    Runs Schedule J calculation in full. Compare ScheduleJForm to outputs in SCHEDULE_J_ALLOCATION_TEST
+    """
     def setUp(self):
         self.test_user = create_test_user(username="test", password="testing")
         self.client.login(username="test", password="testing")
 
     def test_serializer_outputs(self):
         test_years = create_tax_year_data_list(SCHEDULE_J_ALLOCATION_TEST, user=self.test_user, save=True)
-        # Get the dataset instance
-        dataset = test_years[0]['dataset_instance']
-        dataset.save()
 
-        # Convert queryset to a list
-        years = list(TaxYearData.objects.filter(dataset=dataset).order_by("-year"))
-        
-        # Base Calculations
-        for year in years:
-            TaxCalculation(year).calculate()
+        for test_set in test_years:
+            # Get the dataset instance
+            dataset = test_set['dataset_instance']
+            dataset.save()
 
-        sch_j_results = ScheduleJCalculation(years, show_all_years=True, long_form=True).schedule_j_calculation(dataset.max_elected_farm_income, dataset.qualified_farm_income)
-        form = sch_j_results.to_dict()
-        print(form['schedule_j_form'])
-
-        # max_elected, qualified_elected = test["elected"]
-        # dataset = create_dataset_with_tax_years(self.test_user, test['inputs'], max_elected, qualified_elected)
-        # dataset.refresh_from_db()
-
-        # # Base Calculations
-        # for tax_year in TaxYearData.objects.filter(dataset=dataset):
-        #     TaxCalculation(tax_year).calculate()
-
-        # #Schedule J calculation
-        # tax_years = TaxYearData.objects.filter(dataset=dataset).order_by("-year")
-        # sch_j_results = ScheduleJCalculation(tax_years).schedule_j_calculation(dataset.max_elected_farm_income, dataset.qualified_farm_income)
-        # print(sch_j_results.schedule_j_form)
+            # Convert queryset to a list
+            years = list(TaxYearData.objects.filter(dataset=dataset).order_by("-year"))
             
-            # Will need to update the following now that we're no longer using AdjustedTaxData model
+            # Base Calculations
+            for year in years:
+                TaxCalculation(year).calculate()
 
-            # for adjusted in AdjustedTaxData.objects.filter(iteration=i).order_by("-year"):
-            #     sch_j_results.append(adjusted)
+            results_container = ScheduleJCalculation(years, show_all_years=True, long_form=True).schedule_j_calculation(dataset.max_elected_farm_income, dataset.qualified_farm_income)
+            results = results_container.schedule_j_form.to_dict()
+            correct = test_set['outputs']
 
-            # for j, (result, expected) in enumerate(zip(sch_j_results, test['outputs'])):
-            #     if round(result.total_tax) != expected:
-            #         print(f"❌ Test {i}, Year {j}: Got {result.total_tax}, expected {expected}")
-            #         print(f"ordinary: {result.ordinary_tax}, qualified: {result.qualified_tax}")
+            for key in set(results.keys()).union(correct.keys()):
+                val1 = results.get(key, "<missing>")
+                val2 = correct.get(key, "<missing>")
+                if isinstance(val1, Decimal):
+                    val1 = round(float(val1), 0)
+                
+                if val1 != val2:
+                    print(f"Mismatch on key '{key}': calcualtions returned {val1}, correct is {val2}")
+
 
 class ScheduleJOptimizationTest(TestCase):
     def setUp(self):
@@ -361,7 +278,7 @@ class TaxDataSetSerializerTest(APITestCase):
 
 class ElectedIncomeDistributionTest(TestCase):
     """
-    Test to make sure elected income is being distributed properly
+    Make sure elected income is being distributed properly across all years. Need to update to check outputs
     """
     def setUp(self):
         self.test_user = create_test_user(username="test", password="testing")
@@ -375,7 +292,6 @@ class ElectedIncomeDistributionTest(TestCase):
         # List instead of queryset
         years = list(TaxYearData.objects.filter(dataset=dataset).order_by("-year"))
         results = allocate_all_years(years)
-        print(results)           
 
 
 CREDENTIALS = [
@@ -451,40 +367,85 @@ SCHEDULE_J_ALLOCATION_TEST = [
             dict(year=2020, filing_status="MFJ", taxable_income=10000, qualified_income=450, is_electing=False, elected_farm_income=0, qualified_farm_income=0),
             dict(year=2019, filing_status="MFJ", taxable_income=50000, qualified_income=450, is_electing=False, elected_farm_income=0, qualified_farm_income=0),
         ],
-        'outputs': [],
+        'outputs': {
+            'line_1': 120000.00,
+            'line_10': 3333,
+            'line_11': 60000,
+            'line_12': 4903,
+            'line_13': 65000,
+            'line_14': 3333,
+            'line_15': 68333,
+            'line_16': 0.00,
+            'line_17': 18908,
+            'line_18': 18908,
+            'line_19': 10212,
+            'line_2': None,
+            'line_20': 4003,
+            'line_21': 0.00,
+            'line_22': 14215,
+            'line_23': 4692,
+            'line_2a': 10000,
+            'line_2b': 0,
+            'line_2c': None,
+            'line_3': 110000,
+            'line_4': 2892,
+            'line_5': 104333,
+            'line_6': 3333,
+            'line_7': 107667,
+            'line_8': 11112,
+            'line_9': 56667,
+        },
         'dataset': dict(name='Allocation Test Case 1', max_elected_farm_income=10000, qualified_farm_income=0)
     },
-]
-    
-class CalculationsTest(TestCase):
-    def setUp(self):
-        self.test_user = create_test_user(username="test", password="testing")
-        self.client.login(username="test", password="testing")
-
-    def test_base_tax_calculations(self):
-        # Create test sets
-        for i, test in enumerate(TEST):
-            dataset = create_dataset_with_tax_years(self.test_user, test['inputs'], 0, 0)
-
-            total_tax_results = []
-            # Base calculations
-            for tax_year in TaxYearData.objects.filter(dataset=dataset):
-                TaxCalculation(tax_year).calculate()
-                total_tax_results.append(round(tax_year.total_tax))
-
-            # Compare to expected
-            for j, (result, expected) in enumerate(zip(total_tax_results, test['outputs'])):
-                if round(result) != expected:
-                    print(f"❌ Test {i}, Year {j}: Got {result}, expected {expected}")
-
-TEST = [
     {
         'inputs': [
-            [2021, "MFJ", 100000, 40000],
-            [2020, "MFJ", 100000, 40000],
-            [2019, "MFJ", 100000, 40000],
-            [2018, "MFJ", 100000, 40000],
+                dict(year=2023, filing_status="MFJ", taxable_income=120000, qualified_income=105000, is_electing=True, elected_farm_income=10000, qualified_farm_income=0),
+                dict(year=2022, filing_status="MFJ", taxable_income=85000, qualified_income=70000, is_electing=True, elected_farm_income=20000, qualified_farm_income=1000),
+                dict(year=2021, filing_status="single", taxable_income=55000, qualified_income=40000, is_electing=True, elected_farm_income=5000, qualified_farm_income=0),
+                dict(year=2020, filing_status="MFJ", taxable_income=96000, qualified_income=45000, is_electing=False, elected_farm_income=0, qualified_farm_income=0),
+                dict(year=2019, filing_status="MFJ", taxable_income=10000, qualified_income=450, is_electing=False, elected_farm_income=0, qualified_farm_income=0),
+                dict(year=2018, filing_status="MFJ", taxable_income=50000, qualified_income=450, is_electing=False, elected_farm_income=0, qualified_farm_income=0),
+            ],
+            'outputs': {
+                'line_1': 120000,
+                'line_10': 3333,
+                'line_11': 60000,
+                'line_12': 5101,
+                'line_13': 65000,
+                'line_14': 3333,
+                'line_15': 68333,
+                'line_16': 0.00,
+                'line_17': 19948,
+                'line_18': 19948,
+                'line_19': 10335,
+                'line_2': None,
+                'line_20': 4201,
+                'line_21': 0,
+                'line_22': 14536,
+                'line_23': 5412,
+                'line_2a': 10000,
+                'line_2b': 0.00,
+                'line_2c': None,
+                'line_3': 110000,
+                'line_4': 3612,
+                'line_5': 104333,
+                'line_6': 3333,
+                'line_7': 107667,
+                'line_8': 11235,
+                'line_9': 56667,
+            },
+            'dataset': dict(name='Allocation Test Case 1', max_elected_farm_income=10000, qualified_farm_income=0)
+    }
+]
+
+TEST_OLDER_YEARS = [
+    {
+        'inputs': [
+            [2021, "MFJ", 100000, 40000, False, 0, 0],
+            [2020, "MFJ", 100000, 40000, False, 0, 0],
+            [2019, "MFJ", 100000, 40000, False, 0, 0],
+            [2018, "MFJ", 100000, 40000, False, 0, 0],
         ],
-        'outputs': [14253, 1990, 195, 17121],
+        'outputs': [9682, 9805, 9999, 10239],
     },
 ]
